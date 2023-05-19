e1f29fcf332686f455cc55320e21218725fe79e3
usabri <stephen@uyak.me>
2023-05-18 23:43:58 -0400

    Controlling the Frame Rate.

--- assault.py
+++ assault.py
@@ -8,9 +8,10 @@ class ZombieAssault:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
         
         self.screen = pygame.display.set_mode((1920, 1080))
-        pygame.display.set_caption("Zombie Assault v1.0.0")
+        pygame.display.set_caption("Zombie Assault v0.1.0")
         
     def run_game(self):
         """Start the main loop for the game."""
@@ -22,6 +23,7 @@ class ZombieAssault:
                 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
             
 if __name__ == '__main__':
     # Make a game instance and run the game.
