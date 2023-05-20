0f5b82c2f9b645125a2226130a67c90722b1f81b
usabri <133575188+usabri@users.noreply.github.com>
2023-05-20 05:11:21 -0400

    No Need to Store Bullets as a Float.

--- bullet.py
+++ bullet.py
@@ -14,10 +14,7 @@ class Bullet(Sprite):
         # Create a bullet rect at (0,0) and then set correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
                                  self.settings.bullet_height)
-        self.rect.midtop = ai_game.player.rect.midtop
-
-        # Store the bullet's position as a float.
-        self.y = float(self.rect.y)
+        self.rect.center = ai_game.player.rect.center
 
         # Store the bullet's direction.
         self.direction = ai_game.player.direction
@@ -25,28 +22,25 @@ class Bullet(Sprite):
     def update(self):
         """Move the bullet in the direction the player is facing."""
         if self.direction == "up_left":
-            self.x -= self.settings.bullet_speed
-            self.y -= self.settings.bullet_speed
+            self.rect.x -= self.settings.bullet_speed
+            self.rect.y -= self.settings.bullet_speed
         elif self.direction == "up_right":
-            self.x += self.settings.bullet_speed
-            self.y -= self.settings.bullet_speed
+            self.rect.x += self.settings.bullet_speed
+            self.rect.y -= self.settings.bullet_speed
         elif self.direction == "down_left":
-            self.x -= self.settings.bullet_speed
-            self.y += self.settings.bullet_speed
+            self.rect.x -= self.settings.bullet_speed
+            self.rect.y += self.settings.bullet_speed
         elif self.direction == "down_right":
-            self.x += self.settings.bullet_speed
-            self.y += self.settings.bullet_speed
+            self.rect.x += self.settings.bullet_speed
+            self.rect.y += self.settings.bullet_speed
         elif self.direction == "up":
-            self.y -= self.settings.bullet_speed
+            self.rect.y -= self.settings.bullet_speed
         elif self.direction == "down":
-            self.y += self.settings.bullet_speed
+            self.rect.y += self.settings.bullet_speed
         elif self.direction == "left":
             self.rect.x -= self.settings.bullet_speed
         elif self.direction == "right":
             self.rect.x += self.settings.bullet_speed
-
-        # Update the rect position.
-        self.rect.y = self.y
 
     def draw_bullet(self):
         """Draw the bullet to the screen."""
