8f9ae51670f25a19a337e602ee1ff4e20b297de5
usabri <133575188+usabri@users.noreply.github.com>
2023-05-20 01:14:31 -0400

    Refactoring _check_events().

--- assault.py
+++ assault.py
@@ -33,29 +33,36 @@ class ZombieAssault:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.player.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.player.moving_left = True
-                elif event.key == pygame.K_UP:
-                    self.player.moving_up = True
-                elif event.key == pygame.K_DOWN:
-                    self.player.moving_down = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.player.moving_right = False
-                    self.player.image = self.player.player_images["right"]
-                elif event.key == pygame.K_LEFT:
-                    self.player.moving_left = False
-                    self.player.image = self.player.player_images["left"]
-                elif event.key == pygame.K_UP:
-                    self.player.moving_up = False
-                    self.player.image = self.player.player_images["up"]
-                elif event.key == pygame.K_DOWN:
-                    self.player.moving_down = False
-                    self.player.image = self.player.player_images["down"]
-
+                self._check_keyup_events(event)
                 
+    def _check_keydown_events(self, event):
+        """ Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.player.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.player.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.player.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.player.moving_down = True
+            
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.player.moving_right = False
+            self.player.image = self.player.player_images["right"]
+        elif event.key == pygame.K_LEFT:
+            self.player.moving_left = False
+            self.player.image = self.player.player_images["left"]
+        elif event.key == pygame.K_UP:
+            self.player.moving_up = False
+            self.player.image = self.player.player_images["up"]
+        elif event.key == pygame.K_DOWN:
+            self.player.moving_down = False
+            self.player.image = self.player.player_images["down"]
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.blit(self.settings.background, (0, 0))
